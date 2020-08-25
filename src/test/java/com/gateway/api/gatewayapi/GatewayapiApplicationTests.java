c38fadf10372a799bae9f0c1d1bfb89a84052919
unknown <1178332920@qq.com>
2020-08-25 15:33:59 +0800

    first commit

---
+++ src/test/java/com/gateway/api/gatewayapi/GatewayapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.api.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
